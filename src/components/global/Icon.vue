812ed7e16b82257f3854788eb393823aeeea58f4
btkramm <btkramm@uc.cl>
2018-10-09 17:59:29 -0300

    feat(components): added Material Icons support

---
+++ src/components/global/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <i 
+    class="material-icons app-icon"
+    :class="{ button }"
+  >
+    <slot/>
+  </i>
+</template>
+
+<script>
+export default {
+  props: {
+    button: { type: Boolean, default: false },
+  },
+};
+</script>
+
+<style lang="sass">
+.app-icon.button
+  cursor: pointer
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import Icon from './components/global/Icon.vue';
+Vue.component('app-icon', Icon);
 
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
