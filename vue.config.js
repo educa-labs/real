7bfc9c7183f5beb13594cba365f12e549f62b69e
btkramm <btkramm@uc.cl>
2018-10-08 23:17:32 -0300

    feat(setup): clean

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@views': path.resolve(__dirname, 'src/views'),
+      },
+    },
+  },
+};
