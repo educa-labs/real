905375ff0e97d52476cf820efab5eebc5c0fd6c3
btkramm <btkramm@uc.cl>
2018-10-11 01:51:45 -0300

    feat(views): base of Us view

---
+++ src/components/Composed.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-composed">
+    <div
+      v-for="(designer, index) in designers"
+      :key="`designer-${index}`"
+      :style="{ 'background-image': `url(${designer.image})` }"
+      @click="$emit('click', index)"
+    >{{ designer.nickname }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      designers: [
+        { nickname: 'Peli', image: require('@images/image.jpg') },
+        { nickname: 'Cintia', image: require('@images/image.jpg') },
+        { nickname: 'Ferni', image: require('@images/image.jpg') },
+        { nickname: 'Trini', image: require('@images/image.jpg') },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.app-composed
+  div
+    height: 33vh
+    padding: 16px 32px
+
+    +background-image
+    +d-flex(flex-end, flex-start)
+</style>
--- src/views/Us.vue
+++ src/views/Us.vue
@@ -1,3 +1,68 @@
 <template>
-  <div class="app-us"></div>
+  <div class="app-us">
+    <div class="designer">
+      <div class="nickname">{{designer.nickname}}</div>
+      <div class="description">{{designer.description}}</div>
+      <div :style="{ 'background-color': `url(${designer.image}}])` }" class="image"/>
+      <div class="e-mail">{{ designer.eMail }}</div>
+      <app-social-media/>
+    </div>
+    <app-composed @click="selectDesigner"/>
+    <app-footer/>
+  </div>
 </template>
+
+<script>
+import Composed from '@components/Composed';
+import Footer from '@components/Footer';
+import SocialMedia from '@components/SocialMedia';
+
+export default {
+  components: {
+    'app-composed': Composed,
+    'app-footer': Footer,
+    'app-social-media': SocialMedia,
+  },
+  data() {
+    return {
+      designers: [
+        {
+          nickname: 'Peli',
+          description: 'Pellentesque a tempor mi. Sed orci purus, ullamcorper sit amet feugiat sit amet, posuere eu dolor. Integer a diam ipsum. Nulla facilisi. Etiam id vehicula urna. Cras interdum tellus et leo bibendum, vel dignissim lorem rutrum. Aliquam auctor suscipit mauris, feugiat sagittis nunc facilisis in. Mauris leo est, pulvinar ut rhoncus vitae.',
+          image: require('@images/image.jpg'),
+          eMail: 'peli@somosreal.cl',
+        },
+        {
+          nickname: 'Cintia',
+          description: 'Pellentesque a tempor mi. Sed orci purus, ullamcorper sit amet feugiat sit amet, posuere eu dolor. Integer a diam ipsum. Nulla facilisi. Etiam id vehicula urna. Cras interdum tellus et leo bibendum, vel dignissim lorem rutrum. Aliquam auctor suscipit mauris, feugiat sagittis nunc facilisis in. Mauris leo est, pulvinar ut rhoncus vitae.',
+          image: require('@images/image.jpg'),
+          eMail: 'cintia@somosreal.cl',
+        },
+        {
+          nickname: 'Ferni',
+          description: 'Pellentesque a tempor mi. Sed orci purus, ullamcorper sit amet feugiat sit amet, posuere eu dolor. Integer a diam ipsum. Nulla facilisi. Etiam id vehicula urna. Cras interdum tellus et leo bibendum, vel dignissim lorem rutrum. Aliquam auctor suscipit mauris, feugiat sagittis nunc facilisis in. Mauris leo est, pulvinar ut rhoncus vitae.',
+          image: require('@images/image.jpg'),
+          eMail: 'ferni@somosreal.cl',
+        },
+        {
+          nickname: 'Trini',
+          description: 'Pellentesque a tempor mi. Sed orci purus, ullamcorper sit amet feugiat sit amet, posuere eu dolor. Integer a diam ipsum. Nulla facilisi. Etiam id vehicula urna. Cras interdum tellus et leo bibendum, vel dignissim lorem rutrum. Aliquam auctor suscipit mauris, feugiat sagittis nunc facilisis in. Mauris leo est, pulvinar ut rhoncus vitae.',
+          image: require('@images/image.jpg'),
+          eMail: 'trini@somosreal.cl',
+        }
+      ],
+      selected: 0,
+    }
+  },
+  computed: {
+    designer() {
+      return this.designers[this.selected];
+    }
+  },
+  methods: {
+    selectDesigner(index) {
+      this.selected = index
+    }
+  }
+}
+</script>
