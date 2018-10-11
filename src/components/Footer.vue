c0f2fd7642661cc2ef9d4df3a34558d260c2f089
btkramm <btkramm@uc.cl>
2018-10-10 22:56:03 -0300

    feat(components): added SocialMedia and Footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer class="app-footer">
+    <div>
+      <div class="e-mail">{{eMail}}</div>
+      <app-social-media/>
+    </div>
+    <div>
+      <div>{{ addressA }}</div>
+      <div>{{ addressB }}</div>
+      <div>{{ city }}</div>
+      <div>{{ country }}</div>
+    </div>
+    <div>
+      <div>{{ cellPhone }}</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import SocialMedia from '@components/SocialMedia';
+
+export default {
+  components: {
+    'app-social-media': SocialMedia,
+  },
+  data() {
+    return {
+      addressA: 'Máximo Jeria 655',
+      addressB: 'Casa B, Ñuñoa',
+      cellPhone: '56229816946',
+      city: 'Santiago',
+      country: 'Chile',
+      eMail: 'hola@somosreal.cl',
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.app-footer
+  display: grid
+  grid-template-columns: repeat(3, 1fr)
+  justify-items: center
+
+  .e-mail
+    margin-bottom: 32px
+</style>
---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="{ vertical }" class="app-social-media">
+    <img src="@images/instagram.svg">
+    <div class="separator"/>
+    <img src="@images/linkedin.svg">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false,
+    }
+  }
+}
+</script>
+
+
+<style lang="sass">
+$height: 20px
+$margin: 5px
+$width: 2px
+
+.app-social-media
+  display: inline-flex
+  align-items: center
+  justify-content: center
+
+  .separator
+    width: $width
+    height: $height
+    margin: 0 $margin
+    background-color: $c-black
+
+.app-social-media.vertical
+  flex-direction: column
+
+  .separator
+    width: $height
+    height: $width
+
+    margin: $margin 0
+
+.app-social-media img
+  +size(24px)
+</style>
