d04c9ba0670499082f2bd348cf968a40f4e0b644
btkramm <btkramm@uc.cl>
2018-10-10 23:57:03 -0300

    feat(views): added Home view using router-view

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-home">
+    <app-hero/>
+    <app-quote/>
+    <app-skills/>
+    <app-gallery/>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+import Footer from '@components/Footer';
+import Gallery from '@components/Gallery';
+import Hero from '@components/Hero';
+import Quote from '@components/Quote';
+import Skills from '@components/Skills';
+
+export default {
+  components: {
+    'app-footer': Footer,
+    'app-gallery': Gallery,
+    'app-hero': Hero,
+    'app-quote': Quote,
+    'app-skills': Skills,
+  }
+}
+</script>
